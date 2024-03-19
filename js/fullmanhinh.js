c328f023bdbc63251f5e094e782e844f6fbb9803
vdevcode <thanhvumaidao1234567@gmail.com>
2024-03-19 07:14:13 +0700

    upload spin

---
+++ js/fullmanhinh.js
@@ -0,0 +1,16 @@
+// window.onload = function() {
+//     function requestFullscreenForBody() {
+//       var elem = document.documentElement;
+//       if (elem.requestFullscreen) {
+//         elem.requestFullscreen();
+//       } else if (elem.mozRequestFullScreen) { 
+//         elem.mozRequestFullScreen();
+//       } else if (elem.webkitRequestFullscreen) {
+//         elem.webkitRequestFullscreen();
+//       } else if (elem.msRequestFullscreen) {
+//         elem.msRequestFullscreen();
+//       }
+//     }
+
+//     requestFullscreenForBody();
+//   };
